8c1d421795014ac616a7254d0adb01d70a79f085
parag <parag@DESKTOP-I2Q9QKQ>
2021-09-02 11:43:44 +0530

    Add project files.

---
+++ dispose1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace dispose1
+{
+    public class demonstratingclass : IDisposable
+    {
+        public void Dispose()
+        {
+            Console.WriteLine("The dispose() function has been called and the resources have been released!");
+        }
+    }
+    public class demofordispose
+    {
+     
+        public static void Main(string[] args)
+        {
+           
+            var result = new demonstratingclass();
+            
+            result.Dispose();
+        }
+    }
+
+    
+}
